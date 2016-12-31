6e8625db9a30f3507eec93d73cc86b7392c29716
Antoine Gagné <gagnantoine@gmail.com>
2016-12-31 12:13:03 -0500

    Add initial draft

---
+++ README.md
@@ -0,0 +1,17 @@
+# `create-ignore`
+
+This is a short program to generate `.gitignore` files from the `gitignore.io` API.
+
+## Prerequisite
+
+To install this program, you will need the [`stack`](https://docs.haskellstack.org/en/stable/README/) package manager.
+
+## Installation
+
+Assuming you have the `stack` package manager, you can simply run:
+
+```sh
+stack install
+```
+
+Then, you can call the program by calling its name `create-gitignore`.
---
+++ src/Main.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.ByteString.Lazy (ByteString)
+import Data.List (intercalate)
+import Network.HTTP.Client ( parseRequest
+                           , newManager
+                           , httpLbs
+                           , responseBody
+                           )
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Options.Applicative
+
+import qualified Data.ByteString.Lazy as ByteString
+import qualified Data.ByteString.Lazy.Char8 as Char8
+
+data Options = Options { optionCommand :: !Command }
+
+data Command = Fetch [String]
+             | List
+
+main :: IO ()
+main = do
+    options <- execParser optionsParser
+    case optionCommand options of
+      Fetch tools -> fetchGitignore tools >>= Char8.putStrLn
+      List -> listTools >>= Char8.putStrLn
+    where
+        optionsParser = info ( helper <*> 
+                               versionOption <*> 
+                               programOptions
+                             )
+                             ( fullDesc <>
+                               progDesc "Create a .gitignore" <>
+                               header "create-gitignore - A program that executes commands against the `gitignore.io` API."
+                             )
+        versionOption = infoOption "1.0.0.0" (long "version" <> help "Show version")
+        programOptions = Options <$> hsubparser (listCommand <> fetchCommand)
+        listCommand = command "list" (info (pure List) (progDesc "List the different options"))
+        fetchCommand = command "fetch" (info fetchOptions (progDesc "Fetch the corresponding `.gitignore` from the API."))
+        fetchOptions = Fetch <$> many (strArgument (metavar "TOOLS" <> help "Name of the different tools and languages used"))
+
+fetchGitignore :: [String] -> IO ByteString
+fetchGitignore tools = sendRequest fetchUrl
+    where formattedTools = intercalate "," tools
+          fetchUrl = apiUrl ++ formattedTools
+
+listTools :: IO ByteString
+listTools = sendRequest listUrl
+    where listUrl = apiUrl ++ "list"
+
+apiUrl :: String
+apiUrl = "https://www.gitignore.io/api/"
+
+sendRequest :: String -> IO ByteString
+sendRequest url = do
+    manager <- newManager tlsManagerSettings
+    request <- parseRequest url
+    responseBody <$> httpLbs request manager
